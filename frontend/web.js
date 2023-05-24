145b5d7b73721bef2b0b73134d41dfdcd81ca156
Your Name <you@example.com>
2023-05-24 14:04:45 -0300

    agrego diseño de la pagina

---
+++ frontend/web.js
@@ -0,0 +1,10 @@
+const botonqr= document.getElementById("botonqr");
+const qr= document.getElementById("qr");
+
+botonqr.addEventListener("click", function() {
+    qr.innerHTML = "Este es el QR";
+  });
+
+
+
+
